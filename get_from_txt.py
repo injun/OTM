8b3fa7483805a5218952327a7e17b7d013004801
danielmarinha@gmail.com <danielmarinha@gmail.com>
2015-05-12 00:14:17 +0200

    general backup and synchronizing

--- get_from_txt.py
+++ get_from_txt.py
@@ -1,8 +1,19 @@
-import pandas as pd
 from collections import defaultdict
 from itertools import groupby
+import shutil
+import os
+import pandas as pd
 
-results = pd.read_csv('1-200.csv', sep=',', index_col=False)
+
+# set path to working folders
+root_csv = './csv_results/'           # folder containing the query result files *.bib
+dir_top10 = './results/top10'           # output results folder
+if os.path.exists(dir_top10):
+    shutil.rmtree(dir_top10)       # delete results folder if exists
+os.makedirs(dir_top10)             # creates a new, empty one
+
+
+results = pd.read_csv('csv_results/1-200.csv', sep=',', index_col=False)
 # results = results.sort_index(by='PY')   # sort by publication year
 
 timespan = range(2000, 2016)
@@ -27,11 +38,14 @@ for year in timespan:
     frequency_list.sort(key=lambda item: item[1], reverse=True)
     top_10_keywords = frequency_list[:10]   # get only 10 highest counts
     print top_10_keywords
-    with open('top_10_keywords.txt', 'w') as of:
+    with open('results/top10/top_10_keywords.txt', 'w') as of:
         for word in top_10_keywords:
             print word[0]
-            keyword_results = keyword_results + str(year) + ', ' + word[0] + ', ' + word[1] + '\n'
-            outFileKeywordName = year + '-results.csv'
+            current_keyword = str(word[0])
+            current_count = str(word[1])
+            current_year = str(year)
+            keyword_results = keyword_results + current_keyword + ', ' + current_count + '\n'
+            outFileKeywordName = dir_top10 + '_' + current_year + '.csv'
             out_fileKeyword = open(outFileKeywordName, 'a+')
             out_fileKeyword.write(keyword_results)
             out_fileKeyword.close()
