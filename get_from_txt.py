cc92b8727c122ba29cbc6ee968188b9b002e756e
danielmarinha@gmail.com <danielmarinha@gmail.com>
2015-05-11 21:21:45 +0200

    general backup and synchronizing

--- get_from_txt.py
+++ get_from_txt.py
@@ -1,33 +1,42 @@
 import pandas as pd
-from itertools import groupby
 from collections import defaultdict
+from itertools import groupby
 
-results = pd.read_csv('1990-2000.txt', sep='\t', index_col=False)
+results = pd.read_csv('1-200.csv', sep=',', index_col=False)
 # results = results.sort_index(by='PY')   # sort by publication year
 
-#print(type(results.iloc[1]['PY']))
-
-timespan = range(1990, 2001)
+timespan = range(2000, 2016)
 keywords = defaultdict(str)
+frequency = defaultdict(str)
+keyword_results = ''
+
 for year in timespan:
-
     for i in range(1, len(results)):
-        if year == results.iloc[i]['PY']:
-            keywords_string = results.iloc[i]['DE']
-            if isinstance(keywords_string, str):
-                keywords[year] = keywords[year] + ";" + keywords_string.lower()   # Create dictionary of keywords, indexed by year
-print keywords[1991].split(';')
-
+        if year == results.iloc[i]['Year']:
+            author_keywords = results.iloc[i]['Author Keywords']
+            index_keywords = results.iloc[i]['Index Keywords']
+            if isinstance(author_keywords, str):
+                keywords[year] = keywords[year] + ";" + author_keywords.lower()
+            if isinstance(index_keywords, str):
+                keywords[year] = keywords[year] + ';' + index_keywords.lower()
+    keywords[year] = keywords[year].split(';')
 
 # count keyword frequency: top 10
-# result = dict((key, len(list(group))) for key, group in groupby(sorted(words)))     # groups similar instances (key) and returns a list (group), converts list to dictionary
-# l = result.items()                                                                  # convert into a list
-# l.sort(key=lambda item: item[1], reverse=True)
-#
-# # count selected keywords
-# result = dict((key, len(list(group))) for key, group in groupby(sorted(words)))     # groups similar instances (key) and returns a list (group), converts list to dictionary
-#         l = result.items()                                                                  # convert into a list
-#         l.sort(key=lambda item: item[1], reverse=True)
+    frequency[year] = dict((key, len(list(group))) for key, group in groupby(sorted(keywords[year])))     # groups similar instances (key) and returns a list (group), converts list to dictionary
+    frequency_list = frequency[year].items()
+    frequency_list.sort(key=lambda item: item[1], reverse=True)
+    top_10_keywords = frequency_list[:10]   # get only 10 highest counts
+    print top_10_keywords
+    with open('top_10_keywords.txt', 'w') as of:
+        for word in top_10_keywords:
+            print word[0]
+            keyword_results = keyword_results + str(year) + ', ' + word[0] + ', ' + word[1] + '\n'
+            outFileKeywordName = year + '-results.csv'
+            out_fileKeyword = open(outFileKeywordName, 'a+')
+            out_fileKeyword.write(keyword_results)
+            out_fileKeyword.close()
+            keyword_results = ''
+
 # store in result file: year, counts
 #
 # print keywords
